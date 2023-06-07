04be0c420ca6eb4eefeff0797e10947a1e16ae7d
TanyaMalina <105852395+TanyaMalina@users.noreply.github.com>
2023-06-07 22:29:26 +0300

    Add component SuperRange on page JuniorPlus

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,10 +4,36 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
+            sx={{
+                width: '160px',
+                color: '#00CC22',
+                height: '4px',
+                borderRadius: '10px',
+                '& .MuiSlider-track': {
+                    border: 'none'
+                },
+                '& .MuiSlider-thumb': {
+                    width: 20,
+                    height: 20,
+                    border: '1px solid #00CC22',
+                    backgroundColor: '#fff',
+                    '&:before': {
+                        content: '""',
+                        width: '6px',
+                        height: '6px',
+                        borderRadius: '50%',
+                        backgroundColor: '#00CC22',
+                        zIndex: -1
+                    },
+                    '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                        boxShadow: 'none'
+                    }
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#8B8B8B'
+                }
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props}
         />
     )
 }
